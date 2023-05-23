9321a399ec75790672a03471d446fd9c381efa15
jiangyk <jiangyk@nwafu.edu.cn>
2023-05-23 09:09:31 +0800

    引入 admin 监控模块

---
+++ e-commerce-admin/src/main/java/com/jxm/ecommerce/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.jxm.ecommerce;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h1>监控中心服务启动入口</h1>
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
